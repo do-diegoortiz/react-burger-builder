866bcaa00d44e34b78fe84c52e7273990287c252
Diego Ortiz <alejojo23@gmail.com>
2018-09-09 12:55:24 -0500

    Ingredient stateless compoenent converted to stateful in order to use props validations

--- src/components/Burger/Ingredient/Ingredient.js
+++ src/components/Burger/Ingredient/Ingredient.js
@@ -1,29 +1,38 @@
-import React from 'react'
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 import classes from './ingredient.css'
 
-const ingredient = props => {
-  let piece = null
+class Ingredient extends { Component } {
+  render() {
+    let piece = null
 
-  switch (props.type) {
-    case ('bread-bottom'):
-      piece = <div className={classes.BreadBottom}></div>
-      break
-    case ('bread-top'):
-      piece = <div className={classes.BreadTop}></div>
-      break
-    case ('meat'):
-      piece = <div className={classes.Meat}></div>
-      break
-    case ('salad'):
-      piece = <div className={classes.Salad}></div>
-      break
-    case ('bacon'):
-      piece = <div className={classes.Bacon}></div>
-      break
-    default:
-      piece = null
-      break
+    switch (this.props.type) {
+      case ('bread-bottom'):
+        piece = <div className={classes.BreadBottom}></div>
+        break
+      case ('bread-top'):
+        piece = <div className={classes.BreadTop}></div>
+        break
+      case ('meat'):
+        piece = <div className={classes.Meat}></div>
+        break
+      case ('salad'):
+        piece = <div className={classes.Salad}></div>
+        break
+      case ('bacon'):
+        piece = <div className={classes.Bacon}></div>
+        break
+      default:
+        piece = null
+        break
+    }
+
+    return piece
   }
 }
 
-export default ingredient
+Ingredient.propTypes = {
+  type: PropTypes.string.required
+}
+
+export default Ingredient
