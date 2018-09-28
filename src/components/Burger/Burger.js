db986f5052b884426b4e13c1f1839018cc21889b
Diego Ortiz <alejojo23@gmail.com>
2018-09-27 23:09:07 -0500

    Reduced the array of ingredients to get the length and validate if it is zero. If it is I'll show a message in the same variable

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,13 +3,19 @@ import classes from './Burger.css'
 import Ingredient from './Ingredient/Ingredient'
 
 const burger = props => {
-  const ingredientsObjectToArray = Object.keys(props.ingredients)
+  let ingredientsObjectToArray = Object.keys(props.ingredients)
     .map(ingredient => {
       return [...Array(props.ingredients[ingredient])].map((_, index) => {
         return <Ingredient key={ingredient + index} type={ingredient} />
       })
-
     })
+    .reduce((acumulator, current) => {
+      return acumulator.concat(current)
+    }, [])
+    if(ingredientsObjectToArray.length === 0) {
+      ingredientsObjectToArray = <p>Please start adding ingredients</p>
+    }
+    console.log(ingredientsObjectToArray)
   return (
     <div className={classes.Burger}>
       <Ingredient type='bread-top' />
