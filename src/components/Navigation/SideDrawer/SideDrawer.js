b8a4b495b8f2e5018e790b11badfc035780b7ce5
Diego Ortiz <diego.ortiz@youse.com.co>
2018-10-27 11:45:12 -0500

    Added SideDrawer with all its components. Toogle the sidedrawer, adding the logo with height for each use, different flex-flow for the menu items in Desktop and Mobile. Added styles and burger 'logo'

---
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -0,0 +1,31 @@
+import React from 'react'
+
+import Logo from '../../Logo/Logo'
+import NavigationItems from '../NavigationItems/NavigationItems'
+import classes from './SideDrawer.css'
+import Backdrop from '../../UI/Backdrop/Backdrop'
+import Aux from '../../../hoc/Auxiliary'
+
+const sideDrawer = props => {
+  let attachedClases = [classes.SideDrawer, classes.Close]
+  if (props.open) {
+    attachedClases = [classes.SideDrawer, classes.Open]
+  }
+  return (
+    <Aux>
+      <Backdrop show={props.open} clicked={props.closed}/>
+      
+      <div className={attachedClases.join(' ')}>
+        <div className={classes.Logo}>
+          <Logo />
+        </div>
+        <nav>
+
+          <NavigationItems></NavigationItems>
+        </nav>
+      </div>
+    </Aux>
+  )
+}
+
+export default sideDrawer
