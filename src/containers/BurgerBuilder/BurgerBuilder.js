4d16cb74c1d53f5fa670cb6b580c0cf91c25c764
Diego Ortiz <diego.ortiz@youse.com.co>
2018-10-27 12:09:12 -0500

    Change folder structure taking the Layout to the hoc to make more sense

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import Aux from '../../hoc/Auxiliary'
+import Aux from '../../hoc/Auxiliary/Auxiliary'
 import Burger from '../../components/Burger/Burger'
 import Controls from '../../components/Burger/Controls/Controls'
 import Modal from '../../components/UI/Modal/Modal'
